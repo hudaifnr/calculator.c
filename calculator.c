45edb25df6bfff48e782a92c8ce53208e8805554
hudaifnr <102897502+hudaifnr@users.noreply.github.com>
2022-04-23 07:46:59 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -5,7 +5,7 @@ int main() {
     char operation;
     double n1, n2;
 
-    printf("Enter an operator (+, -, *, /): ");
+    printf("Enter an operator: ");
     scanf("%c", &operation);
     printf("Enter two operands: ");
     scanf("%lf %lf",&n1, &n2);
